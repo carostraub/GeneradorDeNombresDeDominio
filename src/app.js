653a60d9255d1b2940cb81267d606b2da991b8c8
carostraub <carolina.straub@gmail.com>
2025-01-22 14:28:31 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        console.log(p + a + n + ".com");
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
